8a7dc2bded2ed0059ce24cd468cbf13961934440
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-12-06 15:49:25 +0530

    basic env setup

---
+++ src/conf/appwrite.config.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteul : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteprojectid : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwritedatabaseid : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwritecollectionid : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwritebucketid : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default conf;
